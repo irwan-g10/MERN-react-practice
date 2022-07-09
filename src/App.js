75ae48461f6e85082c71586e13bbe794d11d6741
Irwan Gumilar <irwangumilar111@gmail.com>
2022-07-09 09:35:04 +0700

    build project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/navigations/MainNavigation";
+
+const App = () => {
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+};
+
+export default App;
